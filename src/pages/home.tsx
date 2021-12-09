8e3ff850eaed23853706e834dc38a14740871c97
jianfeng <gjf610@yeah.net>
2021-12-09 23:22:09 +0800

    make grid

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,9 +2,16 @@ import React from 'react';
 import './home.scss';
 import headerImage from '../images/header.png'
 export const Home = () => {
-    return (
-        <div className="home">
-            <header style={{ backgroundImage: `url(${headerImage})` }}></header>
-        </div>
-    );
+  return (
+    <div className="home">
+      <header style={{ backgroundImage: `url(${headerImage})` }}></header>
+      <main>
+        <section className="section1"></section>
+        <section className="section2"></section>
+        <section className="section3"></section>
+        <section className="section4"></section>
+        <section className="section5"></section>
+      </main>
+    </div>
+  );
 };
