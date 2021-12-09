eb69f79b0770b565bc69c7b07577bacbc798a7cf
jianfeng <gjf610@yeah.net>
2021-12-09 20:19:05 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { HashRouter, Route, Switch } from 'react-router-dom'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <HashRouter>
+        <Switch>
+          <Route>
+            {/* <Home/> */}
+          </Route>
+        </Switch>
+      </HashRouter>
+    </div>
+  )
+}
+
+export default App
