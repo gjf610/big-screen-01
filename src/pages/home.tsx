6e6c319ea3482dfbf16b4aadb849dba6e12abe3d
jianfeng <gjf610@yeah.net>
2021-12-10 21:22:40 +0800

    added Chart6

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,6 +5,9 @@ import { Chart1 } from "../component/chart-1";
 import { Chart2 } from "../component/chart-2";
 import { Chart3 } from "../component/chart-3";
 import { Chart4 } from "../component/chart-4";
+import { Chart5 } from '../component/chart-5';
+import { Chart6 } from '../component/chart-6';
+
 export const Home = () => {
 
   return (
@@ -19,8 +22,13 @@ export const Home = () => {
           <Chart3 />
           <Chart4 />
         </section>
-        <section className="bordered section3"></section>
-        <section className="bordered section4"></section>
+        <section className="bordered section3">
+          <Chart5 />
+        </section>
+        <section className="section4">
+          <Chart6 />
+
+        </section>
         <section className="bordered section5"></section>
       </main>
       <footer>
